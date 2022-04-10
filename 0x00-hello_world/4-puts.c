19323e8f6e48508218f56f461c448691841b3407
ELCUS <amoako30bernard@gmail.com>
2022-04-10 11:58:13 -0700

    puts a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print a string, followed by new line
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
