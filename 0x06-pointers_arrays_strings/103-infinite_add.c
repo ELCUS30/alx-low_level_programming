7872b626609e2fb1f6ed5df6ba2c4c26c996b449
ELCUS <amoako30bernard@gmail.com>
2022-04-25 17:50:57 -0700

    last resortfinale

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,11 +15,11 @@ for (i = 0; n1[i]; i++)
 for (j = 0; n2[j]; j++)
 ;
 if (i > size_r || j > size_r)
-return (0)
+return (0);
 m = 0;
 for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
-n = m
+n = m;
 if (i >= 0)
 n += n1[i] - '0';
 if (j >= 0)
