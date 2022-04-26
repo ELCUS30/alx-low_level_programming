a90e20e52c59ec09fb4451b7ed1349ae89771443
ELCUS <amoako30bernard@gmail.com>
2022-04-25 17:03:59 -0700

    advanced tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,28 @@
  */
 char *cap_string(char *s)
 {
-	 int index = 0;
+int index = 0;
+while (s[index])
+{
+while (!(s[index] >= 'a' && s[index] <= 'z'))
+index++;
+if (s[index - 1] == ' ' ||
+s[index - 1] == '\t' ||
+s[index - 1] == '\n' ||
+s[index - 1] == ',' ||
+s[index - 1] == ';' ||
+s[index - 1] == '.' ||
+s[index - 1] == '!' ||
+s[index - 1] == '?' ||
+s[index - 1] == '"' ||
+s[index - 1] == '(' ||
+s[index - 1] == ')' ||
+s[index - 1] == '{' ||
+s[index - 1] == '}' ||
+index == 0)
+s[index] -= 32;
+index++;
+}
 
-	 while (s[index])
-	 {
-		 while (!(s[index] >= 'a' && s[index] <= 'z'))
-			 index++;
-
-		 if (s[index - 1] == ' ' ||
-				 s[index - 1] == '\t' ||
-				 s[index - 1] == '\n' ||
-				 s[index - 1] == ',' ||
-				 s[index - 1] == ';' ||
-				 s[index - 1] == '.' ||
-				 s[index - 1] == '!' ||
-				 s[index - 1] == '?' ||
-				 s[index - 1] == '"' ||
-				 s[index - 1] == '(' ||
-				 s[index - 1] == ')' ||
-				 s[index - 1] == '{' ||
-				 s[index - 1] == '}' ||
-				 index == 0)
-				 s[index] -= 32;
-
-		 index++;
-	 }
-
-	 return (s);
+return (s);
 }
